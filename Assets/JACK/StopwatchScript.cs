1602145ab05a769a62e7f811918da2bf4fff8a12
Bandj026 <Bandj026@users.noreply.github.com>
2021-04-11 22:38:44 +1000

    updated stopwatch- buttons now functional
    buttons are labelled on the model and are now functional.
    all that's left is the timing script

--- Assets/JACK/StopwatchScript.cs
+++ Assets/JACK/StopwatchScript.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class StopwatchScript : MonoBehaviour
 {
-    //Variable for grabbing the desired actionset
+    /*//Variable for grabbing the desired actionset
     public SteamVR_ActionSet m_ActionSet;
     // varaibles for storing if the touchpads have been used on the controller
     public SteamVR_Action_Vector2 m_TouchPosition;
@@ -18,28 +20,29 @@ public class StopwatchScript : MonoBehaviour
     //location values for the player position and the head position
     private Transform m_CameraRig = null;
     private Transform m_Head = null;
-
-
     //sets the deadzone for how far the player needs to move their thumb before
     public Vector2 m_deadzone = new Vector2(0.1f, 0.1f);
-    public Vector2 m_NeutralPosition = new Vector2(0.0f, 0.0f);
+    public Vector2 m_NeutralPosition = new Vector2(0.0f, 0.0f);*/
+
+
+
     public Text txt;
 
     private void Awake()
     {
-        m_CharacterController = GetComponent<CharacterController>();
+        //m_CharacterController = GetComponent<CharacterController>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        m_CameraRig = SteamVR_Render.Top().origin;
-        m_Head = SteamVR_Render.Top().head;
+        //m_CameraRig = SteamVR_Render.Top().origin;
+        //m_Head = SteamVR_Render.Top().head;
     }
 
     // Update is called once per frame
     void Update()
     {
-        #region Vector2 Action
+        /*#region Vector2 Action
         Vector2 delta = m_TouchPosition[SteamVR_Input_Sources.LeftHand].delta;
         if (delta.x >= (m_NeutralPosition.x + m_deadzone.x) && delta.y >= (m_NeutralPosition.y + m_deadzone.y))
         {
@@ -48,5 +51,11 @@ public class StopwatchScript : MonoBehaviour
         #endregion
 
         txt.text = delta.y.ToString();
+	txt.text = "hi there";*/
+    }
+
+    public void testTime(Hand fromHand)
+    {
+        txt.text = "you did it!";
     }
 }
