3483a734a8a3c0659641ec6451e80f2c209aee4f
tommy-vaux <tommyvaux@icloud.com>
2021-05-01 17:02:50 +1000

    Drawing on whiteboard working with issues

--- Assets/TOMMY/Scripts/Pen.cs
+++ Assets/TOMMY/Scripts/Pen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Pen : MonoBehaviour
@@ -7,6 +8,7 @@ public class Pen : MonoBehaviour
 
     public int penSize;
     public Color penColor;
+    public Color[] color;
 
     private RaycastHit touchPos;
 
@@ -25,8 +27,21 @@ public class Pen : MonoBehaviour
         // send a raycast out to the whiteboard
         if (Physics.Raycast(transform.position, transform.right, out touchPos, 0.01f))
         {
-            Debug.Log("Hit Surface - " + touchPos.collider.gameObject.name);
-            pos.transform.position = touchPos.point;
+            if(touchPos.collider.tag == "Whiteboard_Surface")
+            {
+                //Debug.Log("Hit Surface - " + touchPos.collider.gameObject.name);
+                //pos.transform.position = touchPos.point;
+
+                Whiteboard board = touchPos.collider.GetComponent<Whiteboard>();
+
+                // generate texture blob with correct colour (basically an array of colours, the length of the number of pixels to be drawn)
+                color = Enumerable.Repeat(penColor, penSize * penSize).ToArray();
+
+                Debug.Log("Texture Coords: " + touchPos.textureCoord);
+
+                board.UpdateWhiteboard(touchPos.textureCoord, color, penSize);
+            }
+            
 
         }
         // collision.gameObject.GetComponent<Whiteboard>().UpdateWhiteboard(this.transform.position,penColor, penSize);
--- Assets/TOMMY/Scripts/Whiteboard.cs
+++ Assets/TOMMY/Scripts/Whiteboard.cs
@@ -11,21 +11,69 @@ public class Whiteboard : MonoBehaviour
 
     private Material thisWhiteboard;
 
+    private bool touching, touchingLast;
+
+    private float lastX, lastY;
+
+    Vector2 penPosition;
+    Color[] penColor;
+    int penSize;
+
     private void Start()
     {
-        //thisWhiteboard = new Material(Shader.Find("Standard"));
-
+        thisWhiteboard = new Material(Shader.Find("Standard"));
+        texture = new Texture2D(textureSize, textureSize);
         // generate a new material with the same properties as the default.
-        thisWhiteboard.CopyPropertiesFromMaterial(this.GetComponent<Renderer>().material);
-        this.GetComponent<Renderer>().material = thisWhiteboard;
-
+        Renderer renderer = this.GetComponent<Renderer>(); 
+        thisWhiteboard.CopyPropertiesFromMaterial(renderer.material);
+        renderer.material = thisWhiteboard;
+        renderer.material.mainTexture = (Texture) texture;
+        
     }
 
-    public void UpdateWhiteboard(Vector3 position, Color penColor, int penSize)
+    private void Update()
     {
         // convert coords into pixel values
-        int x = (int)(position.x * textureSize - (penSize / 2));
-        int y = (int)(position.y * textureSize - (penSize / 2));
+        int x = (int)(penPosition.x * textureSize - (penSize / 2));
+        int y = (int)(penPosition.y * textureSize - (penSize / 2));
+
+        Debug.Log(x + "," + y);
+
+        // Only set the pixels if we were touching last frame
+        if (touchingLast)
+        {
+            // Set base touch pixels
+            texture.SetPixels(x, y, penSize, penSize, penColor);
+
+            // Interpolate pixels from previous touch
+            for (float t = 0.01f; t < 1.00f; t += 0.01f)
+            {
+                int lerpX = (int)Mathf.Lerp(lastX, (float)x, t);
+                int lerpY = (int)Mathf.Lerp(lastY, (float)y, t);
+                texture.SetPixels(lerpX, lerpY, penSize, penSize, penColor);
+            }
+        }
+
+        // If currently touching, apply the texture
+        if (touching)
+        {
+            texture.Apply();
+        }
+
+        this.lastX = (float)x;
+        this.lastY = (float)y;
+
+        this.touchingLast = this.touching;
+    }
+
+    public void UpdateWhiteboard(Vector2 position, Color[] color, int size)
+    {
+        penPosition = position;
+        penColor = color;
+        penSize = size;
+
+        touching = true;
+        
     }
 
 }
