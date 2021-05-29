2a3408ffc1d4876fbb9e25395079603a21c15724
Tommy Vaux <tommyvaux@icloud.com>
2021-05-29 15:51:04 +1000

    Added comments

--- Assets/TOMMY/Scripts/DroneFlyPhysics.cs
+++ Assets/TOMMY/Scripts/DroneFlyPhysics.cs
@@ -33,12 +33,14 @@ public class DroneFlyPhysics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // get input from slider
         if(usingSlider)
         {
             movementDir = new Vector2(movementXDir.value, movementZDir.value);
             throttle = engineSpeed.value;
         }
 
+        // apply propellor rotation animation (propellor rotation speed tied to throttle)
         foreach(Transform engine in engines)
         {
             engine.Rotate(Vector3.forward * 15000 * 2.5f * throttle * Time.deltaTime);
@@ -48,13 +50,16 @@ public class DroneFlyPhysics : MonoBehaviour
     // for updating physics (prevents framerate-limited physics issues)
     private void FixedUpdate()
     {
+        // if there is enough throttle and the drone is moving, allow directional movement
         if (rb.velocity.magnitude > 0.1f & throttle > 0.4f)
         {
+            // rotate the drone based on the thumbstick input
             rb.rotation = Quaternion.Euler(movementDir.x * 30, 0, movementDir.y * 30);
         }
 
         foreach (Transform engine in engines)
         {
+            // using physics, add force in the upwards direction from the perspective of each propellor location, with the force amount tied to the throttle.
             rb.AddForceAtPosition(engine.forward * throttle * 4.5f, engine.position);
         }
     }
